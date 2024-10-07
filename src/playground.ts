aa5065fbadf35345a0cecdc481986e51042e100c
沈孟平 <artsmp@126.com>
2024-10-07 14:38:43 +0800

    feat: clerk with webhook and create user to prisma

--- src/playground.ts
+++ src/playground.ts
@@ -1,5 +1,5 @@
 import { db } from "./server/db";
-
+// https://sa-lived-observation-pierre.trycloudflare.com/api/clerk/webook
 await db.user.create({
   data: {
     emailAddress: "test@gmail.com",
